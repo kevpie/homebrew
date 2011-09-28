f09ba2b0bff27bac6cc693c03d30a6c52846191c
Kevin Pierce <kevpie@gmail.com>
2011-09-28 15:58:12 -0600

    adding hardcoded pg_config for now

--- Library/Formula/skytools3.rb
+++ Library/Formula/skytools3.rb
@@ -1,29 +1,26 @@
 require 'formula'
 
 class Skytools3 < Formula
-  head 'https://github.com/kevpie/skytools.git'
-  homepage ''
+ head 'git://github.com/kevpie/skytools.git'
+ homepage ''
 
-  depends_on 'autoconf'
-# symlink autoconf
-  depends_on 'python'
-  depends_on 'postgresql'
-  depends_on 'asciidoc'
-  depends_on 'xmlto'
-  depends_on 'gnu-sed'
+ depends_on 'python'
+ depends_on 'asciidoc'
+ depends_on 'xmlto'
+ depends_on 'gnu-sed'
 
-  def install
-    ENV['XMLTO']='xmlto'
-    ENV['ASCIIDOC']='asciidoc' 
-    system "make boot"
-    system "./configure", "-v", "--with-asciidoc", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+ def install
+   ENV['XMLTO']='xmlto'
+   ENV['ASCIIDOC']='asciidoc'
+   system "make boot"
+   system "./configure", "-v", "--with-asciidoc", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking", "--with-pgconfig=/opt/treadmill/postgresql/bin/pg_config"
 
-    system "make"
-    system "make install"
-  end
+   system "make"
+   system "make install"
+ end
 
-  def test
-    # this will fail we won't accept that, make it test the program works!
-    system "/usr/bin/false"
-  end
+ def test
+   # this will fail we won't accept that, make it test the program works!
+   system "/usr/bin/false"
+ end
 end
